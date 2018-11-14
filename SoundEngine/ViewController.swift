ecf61aa5be6851d7673224d37ef1e47f597ef878
Juhani Vainio <hci.juhani.vainio@gmail.com>
2018-11-14 14:14:21 +0200

    Initial Commit

---
+++ SoundEngine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoundEngine
+//
+//  Created by Juhani Vainio on 14/11/2018.
+//  Copyright © 2018 JuhaniVainio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
